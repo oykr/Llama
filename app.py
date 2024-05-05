9b6ea92b34842e20bb3a9ed28bf706ddf97f7121
oykr <80487035+oykr@users.noreply.github.com>
2024-05-05 17:27:46 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from vllm import LLM, SamplingParams
+from huggingface_hub import snapshot_download
+from pathlib import Path
+
+class InferlessPythonModel:
+    def initialize(self):
+        model_id = "TheBloke/OpenHermes-2.5-Mistral-7B-AWQ"  # Specify the model repository ID
+        # Define sampling parameters for model generation
+        self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=128)
+        # Initialize the LLM object
+        self.llm = LLM(model=model_id, quantization="awq", dtype="float16")
+        
+    def infer(self,inputs):
+        prompts = inputs["prompt"]  # Extract the prompt from the input
+        result = self.llm.generate(prompts, self.sampling_params)
+        # Extract the generated text from the result
+        result_output = [output.outputs[0].text for output in result]
+
+        # Return a dictionary containing the result
+        return {'generated_result': result_output[0]}
+
+    def finalize(self):
+        pass
